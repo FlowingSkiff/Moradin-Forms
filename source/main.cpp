f4b03141d89392c80c6e260430051e088e6dfe2d
FlowingSkiff <boats303@gmail.com>
2022-10-29 13:20:57 -0600

    dependency test

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <string>
+#include <utility>
+
+#include <spdlog/spdlog.h>
+#include <tinyxml2.h>
 
 #include "Core/lib.hpp"
+
+#ifndef AURORA_PATH
+// #  error "AURORA_PATH is not defined"
+#endif
 
 auto main() -> int
 {
   library lib;
   std::string message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
+
   return 0;
 }
